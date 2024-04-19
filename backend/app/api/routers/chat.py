efdfdc69be96a0598a7028af0feb5050e74a5a6e
Blaise <jetblaise@gmail.com>
2024-04-18 21:50:36 -0400

    Add Vectara configuration and setup LLM and retriever***
    ***Update chat engine and response formulation instructions***
    
    ***Update index creation and loading process***
    
    ***Remove unused code and comments

--- backend/app/api/routers/chat.py
+++ backend/app/api/routers/chat.py
@@ -9,6 +9,21 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core.llms import ChatMessage, MessageRole
 from pydantic import BaseModel
 
+from llama_index.llms.together import TogetherLLM
+from llama_index.indices.managed.vectara import VectaraIndex
+from llama_index.indices.managed.vectara import VectaraAutoRetriever
+
+import os
+
+# Vectara Configuration
+VECTARA_CORPUS_ID = os.getenv("VECTARA_CORPUS_ID")
+VECTARA_CUSTOMER_ID = os.getenv("VECTARA_CUSTOMER_ID")
+VECTARA_API_KEY = os.getenv("VECTARA_API_KEY")
+
+
+# Replace 'your_api_key' with the actual API key obtained from Together AI
+TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
+
 
 chat_router = r = APIRouter()
 
@@ -51,16 +66,59 @@ async def chat(
         for m in data.messages
     ]
 
+    # Setup the LLM
+    # llm = TogetherLLM(api_key=TOGETHER_API_KEY, model="meta-llama/Llama-2-70b-chat-hf")
+    # setup index
+    # ara_corpus_id="2",
+    # )
+
+    # Setup the retriever
+    # retriever = VectaraAutoRetriever(
+    # index=index,
+    # llm=llm,
+    # verbose=False,
+    # )
+
     # query chat engine
     chat_engine = index.as_chat_engine(
+        llm=TogetherLLM(
+            api_key=TOGETHER_API_KEY, model="meta-llama/Llama-2-70b-chat-hf"
+        ),
         chat_mode="condense_plus_context",
         context_prompt=(
-            "You are a longevity assistant"
-            "Here are the relevant documents for the context:\n"
-            "{context_str}"
-            "\nInstruction: Use the previous chat history, or the context above, to interact and help the user."
+            "Objective: You serve as the Longevity Assistant, tasked with enhancing users' longevity and healthspan by providing personalized advice. Your guidance should be concise, precise, informed by specific user information and reliable data sources.\n\n"
+            "Instructions:\n\n"
+            "Initial Data Analysis:\n"
+            "Begin by examining the information provided through  - details such as age, health conditions, lifestyle preferences, and health goals.\n"
+            "Simultaneously, utilize the scientific data from Vectara’s 'longevity' corpus available to support your recommendations: {context_str} .\n\n"
+            "Response Formulation:\n"
+            "Combine insights from both data sources to tailor advice specifically suited to each user's needs. This should ensure recommendations are not only personalized but also deeply rooted in the latest research.\n\n"
+            "Tone and Professionalism:\n"
+            "Throughout the interaction, maintain a compassionate and professional tone. Strive to create an environment that reflects empathy and respect, ensuring all users feel valued and supported.\n\n"
+            "Privacy and Ethics:\n"
+            "Handle all personal data with the highest level of confidentiality. Use this information to guide your advice indirectly, ensuring no direct references to sensitive details are made in your responses.\n"
+            "Commit to ethical standards by promoting healthy, sustainable practices for longevity.\n\n"
+            "Output Specifications:\n"
+            "Keep responses concise, within a 500-word limit, to provide clear, actionable advice.\n"
+            "Include up to three specific recommendations that leverage both the personal and scientific data effectively.\n"
+            "Consistently check that the response tone is supportive and nurturing.\n"
+            "Format responses in plain text—avoid using markdown styles or emojis to uphold professionalism.\n\n"
+            "Example Use Case:\n"
+            "User Data: 'age: 52, health conditions: hypertension and diabetes, lifestyle preferences: vegetarian and enjoys walking, health goals: reduce blood pressure and manage blood sugar'\n\n"
+            "Expected Response:\n"
+            "'Considering your proactive health goals and the insights from our longevity studies, here are several strategies to consider:\n"
+            "Integrate a daily 30-minute walk to effectively manage blood pressure and blood sugar levels.\n"
+            "Follow a balanced diet rich in plant proteins and low-glycemic foods to support your vegetarian lifestyle and health conditions.\n"
+            "Regularly consult your healthcare provider to tailor and refine your health plan.\n"
+            "Keep up the great work—consistent efforts are key to achieving better health outcomes!'"
         ),
-        verbose=True,
+        # context_prompt=(
+        # "You are a longevity assistant"
+        # "Here are the relevant documents for the context:\n"
+        # "{context_str}"
+        # "\nInstruction: Use the previous chat history, or the context above, to interact and help the user."
+        # ),
+        verbose=False,
     )
     response = chat_engine.stream_chat(lastMessage.content, messages)
 
--- backend/app/utils/index.py
+++ backend/app/utils/index.py
@@ -9,7 +9,12 @@ from llama_index.core import (
 )
 from llama_index.llms.together import TogetherLLM
 
-# from llama_index.embeddings.together import TogetherEmbedding
+from llama_index.indices.managed.vectara import VectaraIndex
+from llama_index.indices.managed.vectara import VectaraAutoRetriever
+
+
+from llama_index.embeddings.together import TogetherEmbedding
+
 # from llama_index.indices.managed.vectara import VectaraIndex
 
 # Vectara Configuration
@@ -19,7 +24,7 @@ VECTARA_API_KEY = os.getenv("VECTARA_API_KEY")
 
 
 # Replace 'your_api_key' with the actual API key obtained from Together AI
-TOGETHER_API_KEY = os.environ.get("TOGETHER_API_KEY", "your_api_key")
+TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
 
 STORAGE_DIR = "./storage"  # directory to cache the generated index
 DATA_DIR = "./data"  # directory containing the documents to index
@@ -27,9 +32,9 @@ DATA_DIR = "./data"  # directory containing the documents to index
 # Setup LLM and Embedding Model from Together AI
 service_context = ServiceContext.from_defaults(
     llm=TogetherLLM(api_key=TOGETHER_API_KEY, model="meta-llama/Llama-2-70b-chat-hf"),
-    # embed_model=TogetherEmbedding(
-    # api_key=TOGETHER_API_KEY, model_name="togethercomputer/m2-bert-80M-8k-retrieval"
-    # ),
+    embed_model=TogetherEmbedding(
+        api_key=TOGETHER_API_KEY, model_name="togethercomputer/m2-bert-80M-8k-retrieval"
+    ),
 )
 
 
@@ -40,19 +45,41 @@ def get_index():
         # logger.info("Creating new index")
         # load the documents and create the index
         # documents = SimpleDirectoryReader(DATA_DIR).load_data()
-        # index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+        index = VectaraIndex()
         # store it for later
-        index.storage_context.persist(STORAGE_DIR)
-        logger.info(f"Finished creating new index. Stored in {STORAGE_DIR}")
+        # index.storage_context.persist(STORAGE_DIR)
+        # logger.info(f"Finished creating new index. Stored in {STORAGE_DIR}")
     else:
         # load the existing index
         logger.info(f"Loading index from {STORAGE_DIR}...")
-        storage_context = StorageContext.from_defaults(persist_dir=STORAGE_DIR)
-        index = load_index_from_storage(
-            storage_context, service_context=service_context
-        )
+        # storage_context = StorageContext.from_defaults(persist_dir=STORAGE_DIR)
+        index = VectaraIndex()
+        # index = VectaraIndex.as_chat_engine(
+        # llm=TogetherLLM(
+        # api_key=TOGETHER_API_KEY, model="meta-llama/Llama-2-70b-chat-hf"
+        # ),
+        # )
         logger.info(f"Finished loading index from {STORAGE_DIR}")
     return index
 
+
+# def setup_vectara_retriever():
+#     # Setup the LLM
+#     llm = TogetherLLM(api_key=TOGETHER_API_KEY, model="meta-llama/Llama-2-70b-chat-hf")
+#     # setup index
+#     index = VectaraIndex(
+#         vectara_api_key=VECTARA_API_KEY,
+#         vectara_customer_id=VECTARA_CUSTOMER_ID,
+#         vectara_corpus_id="2",
+#     )
+
+#     # Setup the retriever
+#     retriever = VectaraAutoRetriever(
+#         index=index,
+#         llm=llm,
+#         verbose=False,
+#     )
+
+#     return retriever
 
 # Be sure to set your TOGETHER_API_KEY in your environment or directly in the code before running it.
